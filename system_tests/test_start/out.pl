449f454434882562e78503e105f07fe7eddc24ff
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-10 14:19:51 +0300

    Create out.pl

---
+++ system_tests/test_start/out.pl
@@ -0,0 +1,18 @@
+{<|X -> john|>}
+{<|X -> john, Y -> bob|>, <|X -> bob, Y -> david|>}
+false
+{<|Y -> david|>}
+true
+false
+true
+false
+false
+false
+{<|Z -> b, Y -> w, X -> r|>}
+{<|X -> <|Compound -> a, Arguments -> {b, c}|>|>}
+{<|Y -> {q, w, e, r}|>}
+{<|X -> q, Y -> {w, e, r}|>}
+true
+true
+false
+true
