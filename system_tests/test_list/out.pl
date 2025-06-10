27b84fd06193262f3bf88d06133f4c981a9440a7
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-10 21:49:41 +0300

    Create out.pl

---
+++ system_tests/test_list/out.pl
@@ -0,0 +1,14 @@
+{<|X -> a|>, <|X -> b|>, <|X -> c|>}
+true
+false
+false
+true
+true
+false
+{<|X -> <|ListHead -> c, Tail -> {b}|>|>}
+{<|X -> <|ListHead -> c, Tail -> <|ListHead -> <|Compound -> a, Arguments -> {b}|>, Tail -> {}|>|>|>}
+{<|X -> {}|>}
+{<|X -> <|ListHead -> a, Tail -> <|ListHead -> b, Tail -> {c, d, e}|>|>|>}
+false
+{<|Y -> <|ListHead -> a, Tail -> <|ListHead -> b, Tail -> {}|>|>|>}
+{<|Z -> {c, d, e}|>}
