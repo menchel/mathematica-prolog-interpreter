289f175afd2c40e4fb5e2f212e385e9728487ebe
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-10 20:53:46 +0300

    Create out.pl

---
+++ system_tests/test_length/out.pl
@@ -0,0 +1,8 @@
+{<|T -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>}|>|>}
+false
+true
+true
+{<|X -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>}|>|>}
+false
+true
+false
