e0efb5e40c7d4f3074352d9dc3d4f64663e6d4b4
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-10 14:30:55 +0300

    Create out.pl

---
+++ system_tests/simple_facts_tests/out.pl
@@ -0,0 +1,15 @@
+true
+false
+{<|X -> b|>}
+true
+true
+{<|X -> <|Compound -> a, Arguments -> {b}|>|>}
+{<|X -> b|>}
+false
+true
+{<|X -> b|>, <|X -> d|>}
+false
+true
+{<|X -> {a, b, c}|>, <|X -> {a, d, c}|>}
+{<|X -> {b, c}|>, <|X -> {d, c}|>}
+true
