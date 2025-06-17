ad9bfa40bc7abbde43cb0185a396cfbe3bfd8136
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-17 17:56:02 +0300

    Update in.pl

--- system_tests/bt_test/in.pl
+++ system_tests/bt_test/in.pl
@@ -4,11 +4,22 @@ member(X,[Y|Ys]):-member(X, Ys).
 tree(a,[b,c]).
 tree(b,[d,e]).
 tree(c,[k]).
-tree(d,[]).
+tree(d,[r,t]).
 tree(e,[]).
 tree(k,[]).
+tree(t,[]).
+tree(r,[]).
+
+parent(X,Y):-tree(X,L),member(Y,L).
 
 path(X,X).
 path(X,Y):-tree(X,L),member(Z,L),path(Z,Y).
 
+?-parent(a,b).
+?-parent(a,d).
+?-+parent(k,a).
 ?-path(b,k).
+?-path(a,X).
+?-path(b,X).
+?-path(a,c).
+?-+path(a,c).
