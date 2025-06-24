bb8f5cb1282175260bd2adcede4b5491adce096b
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-24 11:58:48 +0300

    Update out.pl

--- system_tests/tree_test/out.pl
+++ system_tests/tree_test/out.pl
@@ -1,13 +1,13 @@
-true
-false
-true
-false
-{<|X -> a|>, <|X -> b|>, <|X -> d|>, <|X -> r|>, <|X -> t|>, <|X -> e|>, <|X -> c|>, <|X -> k|>}
-{<|X -> b|>, <|X -> d|>, <|X -> r|>, <|X -> t|>, <|X -> e|>}
-true
-false
-{<|X -> a, Y -> zero|>, <|X -> b, Y -> <|Compound -> s, Arguments -> {zero}|>|>, <|X -> d, Y -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>|>, <|X -> r, Y -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>}|>|>, <|X -> t, Y -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>}|>|>, <|X -> e, Y -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>|>, <|X -> c, Y -> <|Compound -> s, Arguments -> {zero}|>|>, <|X -> k, Y -> <|Compound -> s, Arguments -> {<|Compound -> s, Arguments -> {zero}|>}|>|>}
-{<|X -> d|>, <|X -> e|>, <|X -> k|>}
-true
-false
-true
+true.
+false.
+true.
+false.
+X = a ; X = b ; X = d ; X = r ; X = t ; X = e ; X = c ; X = k.
+X = b ; X = d ; X = r ; X = t ; X = e.
+true.
+false.
+X = a, Y = zero ; X = b, Y = s(zero) ; X = d, Y = s(s(zero)) ; X = r, Y = s(s(s(zero))) ; X = t, Y = s(s(s(zero))) ; X = e, Y = s(s(zero)) ; X = c, Y = s(zero) ; X = k, Y = s(s(zero)).
+X = d ; X = e ; X = k.
+true.
+false.
+true.
