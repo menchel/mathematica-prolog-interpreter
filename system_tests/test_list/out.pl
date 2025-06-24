989cd7fae29c41a5a2d54e58f179775814b6375f
Inbar Menchel <118845141+menchel@users.noreply.github.com>
2025-06-24 11:53:53 +0300

    Update out.pl

--- system_tests/test_list/out.pl
+++ system_tests/test_list/out.pl
@@ -1,14 +1,14 @@
-{<|X -> a|>, <|X -> b|>, <|X -> c|>}
-true
-false
-false
-true
-true
-false
-{<|X -> <|ListHead -> c, Tail -> {b}|>|>}
-{<|X -> <|ListHead -> c, Tail -> <|ListHead -> <|Compound -> a, Arguments -> {b}|>, Tail -> {}|>|>|>}
-{<|X -> {}|>}
-{<|X -> <|ListHead -> a, Tail -> <|ListHead -> b, Tail -> {c, d, e}|>|>|>}
-false
-{<|Y -> <|ListHead -> a, Tail -> <|ListHead -> b, Tail -> {}|>|>|>}
-{<|Z -> {c, d, e}|>}
+X = a ; X = b ; X = c.
+true.
+false.
+false.
+true.
+true.
+false.
+X = [c, b].
+X = [c, a(b)].
+X = [].
+X = [a, b, c, d, e].
+false.
+Y = [a, b].
+Z = [c, d, e].
